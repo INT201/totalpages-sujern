dd5e7ea1ec66d7a6f49eb78e98e7eedf50754b3b
sujern <sujern7856@gmail.com>
2022-09-27 20:42:37 +0700

    homeworktotalPages

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,21 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if(rowsPerPage == null || undefined){
+    
+    return 1
+  }
+  else if(arrayItems == null || undefined){
+    
+    return undefined
+  }
+  else{
+    page = arrayItems.length/rowsPerPage
+    if(page != Infinity){
+      return Math.ceil(page)
+    }
+    else{return 1}
+   
+  }
 }
 module.exports = totalPages
